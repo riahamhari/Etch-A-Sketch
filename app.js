dc013e4a77122d352fada54e3078437695954a49
riahamhari <109351021+riahamhari@users.noreply.github.com>
2022-09-21 23:32:21 +0100

    options for different sizes

--- app.js
+++ app.js
@@ -1,74 +1,97 @@
+const DEFAULT_COLOR = '#333333'
+const DEFAULT_MODE = 'color'
+const DEFAULT_SIZE = 16
 
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
+
+
+
+
+// Components
+const container = document.querySelector('.container');
 const sizeSlider = document.querySelector('#sizeSlider');
 const sizeValue = document.querySelector('#sizeValue');
-let numSquares = Math.pow(sizeSlider.value, 2) 
+
+
+// Sketch Buttons/Tools
+const colorPicker = document.querySelector('#colorPicker');
+const colorBtn = document.querySelector('#colorBtn')
+const eraserBtn = document.querySelector('#eraserBtn');
+const rainbowBtn = document.querySelector('#rainbowBtn')
+const clearBtn = document.querySelector('#clearBtn');
 
 
 
-sizeSlider.addEventListener('mousemove', function(){
-    sizeValue.innerText = `${sizeSlider.value} x ${sizeSlider.value}`;
-   
+eraserBtn.addEventListener('click', function () {
+    let numSquares = Math.pow(sizeSlider.value, 2)
+    for (let i = 0; i < numSquares; i++) {
+        let eachSquare = document.querySelectorAll('.square');
+        eachSquare[i].addEventListener('mouseover', function () {
+            eachSquare[i].style.backgroundColor = 'white'
+        })
+
+
+    }
+
 })
 
-// sizeSlider.addEventListener('change', function(){
-//     sizeValue.innerText = `${sizeSlider.value} x ${sizeSlider.value}`;
-//     for (let i = 0; i < numSquares; i++) {
-//         let square = document.querySelectorAll('.square')
-//         square[i].remove()
-//     }
-// generateGrid();
-// })
 
 
-function randColor (){
-    let r = Math.floor(Math.random()* 255 + 1)
-    let g = Math.floor(Math.random()* 255 + 1)
-    let b = Math.floor(Math.random()* 255 + 1)
+
+sizeSlider.addEventListener('mousemove', function () {
+    sizeValue.innerText = `${sizeSlider.value} x ${sizeSlider.value}`;
+
+})
+
+sizeSlider.addEventListener('mouseup', function () {
+    let square = document.querySelectorAll('.square')
+    for (let i = 0; i < square.length; i++) {
+
+        square[i].parentNode.removeChild(square[i]);
+
+
+
+    }
+    generateGrid()
+
+
+
+
+})
+
+
+
+
+function randColor() {
+    let r = Math.floor(Math.random() * 255 + 1)
+    let g = Math.floor(Math.random() * 255 + 1)
+    let b = Math.floor(Math.random() * 255 + 1)
     return `rgb(${r}, ${g}, ${b})`
 }
 
 
-function generateGrid(){
-for (let i = 0; i < numSquares; i++) {
-    let square = document.createElement('div');
-    let container = document.querySelector('.container');
+function generateGrid() {
+    let numSquares = Math.pow(sizeSlider.value, 2)
 
-    square.classList.add('square');
-    square.style.width = `${480/sizeSlider.value}px`
-    square.style.height = `${480/sizeSlider.value}px`
-    container.append(square);
+    for (let i = 0; i < numSquares; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${480 / sizeSlider.value}px`
+        square.style.height = `${480 / sizeSlider.value}px`
+        container.append(square);
 
-    let eachSquare = document.querySelectorAll('.square');
-    eachSquare[i].addEventListener('mouseover', function(){
-        eachSquare[i].style.backgroundColor = randColor()})
-    // eachSquare[i].addEventListener('mousemove', function(){
-    //     eachSquare[i].style.backgroundColor = randColor()})
-    // eachSquare[i].addEventListener('mouseup', function(){
-    //     eachSquare[i].style.backgroundColor = randColor()})
-    
-    
+        let eachSquare = document.querySelectorAll('.square');
+        eachSquare[i].addEventListener('mouseover', function () {
+            eachSquare[i].style.backgroundColor = randColor()
+        })
+
+    }
+
+
 }
-
-
-    
-}
-
 
 
 generateGrid();
 
-// function hover (){
-//     eachSquare
-
-// }
-
-
-
-
-// function color() {
-    
-// }
-
-// container.addEventListener('click', function () {
-    
-// })
