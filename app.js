7dfc8e5c11d7131363526353cc98ba168abd63aa
riahamhari <109351021+riahamhari@users.noreply.github.com>
2022-09-28 00:03:19 +0100

    media queries and styling update

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ let currentMode = DEFAULT_MODE
 let currentSize = DEFAULT_SIZE
 
 
+function setCurrentColor(newColor) {
+    currentColor = newColor
+}
+
+function setCurrentMode(newMode) {
+    activateButton(newMode)
+    currentMode = newMode
+}
+
+function setCurrentSize(newSize) {
+    currentSize = newSize
+}
+
 
 
 // Components
@@ -22,76 +35,104 @@ const eraserBtn = document.querySelector('#eraserBtn');
 const rainbowBtn = document.querySelector('#rainbowBtn')
 const clearBtn = document.querySelector('#clearBtn');
 
+colorPicker.oninput = (e) => setCurrentColor(e.target.value)
+colorBtn.onclick = () => setCurrentMode('color')
+rainbowBtn.onclick = () => setCurrentMode('rainbow')
+eraserBtn.onclick = () => setCurrentMode('eraser')
+clearBtn.onclick = () => reloadGrid()
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value)
 
-
-eraserBtn.addEventListener('click', function () {
-    let numSquares = Math.pow(sizeSlider.value, 2)
-    for (let i = 0; i < numSquares; i++) {
-        let eachSquare = document.querySelectorAll('.square');
-        eachSquare[i].addEventListener('mouseover', function () {
-            eachSquare[i].style.backgroundColor = 'white'
-        })
-
-
-    }
-
-})
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 
 
 
-sizeSlider.addEventListener('mousemove', function () {
-    sizeValue.innerText = `${sizeSlider.value} x ${sizeSlider.value}`;
+function updateSizeValue(value) {
+    sizeValue.innerText = `${value} x ${value}`
+}
 
-})
+function changeSize(value) {
+    setCurrentSize(value)
+    updateSizeValue(value)
+    reloadGrid()
 
-sizeSlider.addEventListener('mouseup', function () {
+}
+
+
+
+function reloadGrid() {
     let square = document.querySelectorAll('.square')
     for (let i = 0; i < square.length; i++) {
 
         square[i].parentNode.removeChild(square[i]);
-
-
-
     }
-    generateGrid()
-
-
-
-
-})
-
-
-
-
-function randColor() {
-    let r = Math.floor(Math.random() * 255 + 1)
-    let g = Math.floor(Math.random() * 255 + 1)
-    let b = Math.floor(Math.random() * 255 + 1)
-    return `rgb(${r}, ${g}, ${b})`
+    generateGrid(sizeSlider.value)
 }
 
 
-function generateGrid() {
-    let numSquares = Math.pow(sizeSlider.value, 2)
+
+
+
+function randColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (currentMode === 'rainbow') {
+        let r = Math.floor(Math.random() * 255 + 1)
+        let g = Math.floor(Math.random() * 255 + 1)
+        let b = Math.floor(Math.random() * 255 + 1)
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    } else if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = '#fefefe'
+    }
+}
+
+
+function generateGrid(size) {
+    let numSquares = Math.pow(size, 2)
 
     for (let i = 0; i < numSquares; i++) {
-        let square = document.createElement('div');
+        const square = document.createElement('div');
         square.classList.add('square');
-        square.style.width = `${480 / sizeSlider.value}px`
-        square.style.height = `${480 / sizeSlider.value}px`
+        square.style.width = `${parseInt(window.getComputedStyle(container).width) / sizeSlider.value}px`
+        square.style.height = `${parseInt(window.getComputedStyle(container).height) / sizeSlider.value}px`
         container.append(square);
 
         let eachSquare = document.querySelectorAll('.square');
-        eachSquare[i].addEventListener('mouseover', function () {
-            eachSquare[i].style.backgroundColor = randColor()
-        })
+        eachSquare[i].addEventListener('mouseover', randColor)
+        eachSquare[i].addEventListener('mousedown', randColor)
+
 
     }
 
 
 }
 
+function activateButton(newMode) {
+    if (currentMode === 'rainbow') {
+        rainbowBtn.classList.remove('active')
+    } else if (currentMode === 'color') {
+        colorBtn.classList.remove('active')
+    } else if (currentMode === 'eraser') {
+        eraserBtn.classList.remove('active')
+    }
 
-generateGrid();
+    if (newMode === 'rainbow') {
+        rainbowBtn.classList.add('active')
+    } else if (newMode === 'color') {
+        colorBtn.classList.add('active')
+    } else if (newMode === 'eraser') {
+        eraserBtn.classList.add('active')
+    }
+}
+
+
+window.onload = () => {
+    generateGrid(DEFAULT_SIZE)
+    activateButton(DEFAULT_MODE)
+}
+
 
